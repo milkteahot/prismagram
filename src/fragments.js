4b6e6653e4b487c4f3afd6b8e627a8b84d71268b
milkteahot <greenegoode11@gmail.com>
2020-02-11 11:11:57 +0000

    send,recieve message

--- src/fragments.js
+++ src/fragments.js
@@ -8,17 +8,32 @@ export const COMMENT_FRAGMENT = `
 export const USER_FRAGMENT = `
         id
         userName
+        avatar
 `
 export const FILE_FRAGMENT = `
         id
         url
+`
+export const MESSAGE_FRAGMENT = `
+        id
+        text
+        to {
+                ${USER_FRAGMENT}
+        }       
+        from {
+                ${USER_FRAGMENT}
+        } 
 `
 
 export const ROOM_FRAGMENT = `
         fragment RoomParts on Room {
                 id
                 participants {
-                        id
+                        ${USER_FRAGMENT}
+                }
+                messages {
+                        ${MESSAGE_FRAGMENT}
                 }
         }
 `
+
