342592cc7d0af02325007775cb0268429b821db7
milkteahot <greenegoode11@gmail.com>
2020-02-26 22:11:57 +0900

    prisma.yml

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,19 @@ export default {
     Mutation:{
         createAccount: async(_, args) => {
             const { userName, email, firstName="", lastName="", bio="" } = args;
+            const exists = await prisma.$exists.user({userName});
+            if(exists) {
+                throw Error("This username is already taken");
+            }
+                await prisma.createUser({
+                    userName,
+                    email,
+                    firstName,
+                    lastName,
+                    bio
+                });
+                return true;
+        
             // const user = await prisma.createUser({
             //     userName, 
             //     email, 
@@ -11,7 +24,7 @@ export default {
             //     lastName, 
             //     bio
             // });
-            return user;
+            //return user;
         }
     }
 }
