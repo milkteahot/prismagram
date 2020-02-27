4647c254f0ffcde8ac3576eb0273f5f30f589d55
milkteahot <greenegoode11@gmail.com>
2020-02-27 20:02:00 +0900

    update package.json

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,9 +4,14 @@ export default {
     Mutation:{
         createAccount: async(_, args) => {
             const { userName, email, firstName="", lastName="", bio="" } = args;
-            const exists = await prisma.$exists.user({userName});
+            const exists = await prisma.$exists.user({
+                OR: [
+                    {userName},
+                    {email}
+                ]
+            });
             if(exists) {
-                throw Error("This username is already taken");
+                throw Error("This username/email is already taken");
             }
                 await prisma.createUser({
                     userName,
