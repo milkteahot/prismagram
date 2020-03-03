2e77d4296c2b3d0198d3f1e3b11569deb14d1b3b
milkteahot <greenegoode11@gmail.com>
2020-03-04 04:27:55 +0900

    add tag edit

--- src/api/Member/editMember/editMember.js
+++ src/api/Member/editMember/editMember.js
@@ -7,13 +7,13 @@ export default {
     Mutation: {
         editMember: async(_, args, {request, isAuthenticated}) => {
             isAuthenticated(request);
-            const { id, memberName, action } = args;
+            const { id, memberName, action, mtag1, mtag2, mtag3 } = args;
             const { user } = request;
             const member = await prisma.$exists.member({ id, user: {id: user.id }});
             if(member) {
                 if(action === EDIT) {
                     return prisma.updateMember(
-                        {data: {memberName}, 
+                        {data: {memberName, mtag1, mtag2, mtag3}, 
                         where: {id}
                         }); 
                 } else if(action === DELETE) {
