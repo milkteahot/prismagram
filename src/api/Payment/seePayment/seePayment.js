994aa8a3d95da3f94fcde710eb54dc74d4177f88
milkteahot <greenegoode11@gmail.com>
2020-03-15 03:28:31 +0900

    seePayment query

---
+++ src/api/Payment/seePayment/seePayment.js
@@ -0,0 +1,17 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query: {
+        seePayment : (_, __, { request, isAuthenticated }) => {
+            isAuthenticated(request);
+            const { user } = request;
+            return prisma.payments({
+                where: {
+                    user: {
+                        id: user.id
+                    }
+                }
+            })
+        }
+    }
+}
