2565ffed73b17bc6f2e8da2f5424e4ef4512fd39
milkteahot <greenegoode11@gmail.com>
2020-03-29 18:48:00 +0900

    seeFeedAll 추가

---
+++ src/api/Payment/returnPayment/returnPayment.js
@@ -0,0 +1,81 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Mutation: {
+        returnPayment : async (_, args, { request }) => {
+            const { user } = request;
+            const { product, count, cart } = args; 
+
+            if(cart === undefined) {
+                try {
+                    await count.map(async(item, index) => {
+                        const countId = await prisma.createCount({
+                            count: item
+                        }); 
+                        const payment = await prisma.createPayment({
+                            user: {
+                                connect: {
+                                    id: user.id 
+                                }
+                            }, 
+                            product: {
+                                connect: {
+                                    id: product[index]
+                                }
+                            }, 
+                            count: {
+                                connect: {
+                                    id: countId.id
+                                }
+                            },
+                        });
+                        console.log(payment);
+                    })
+                    // return true;
+                    // return payment;
+                } catch (error) {
+                    console.log(error);
+                    return false; 
+                }
+            } else {
+                try {
+                    await count.map(async(item, index) => {
+                        const countId = await prisma.createCount({
+                            count: item
+                        }); 
+                        const payment = await prisma.createPayment({
+                            user: {
+                                connect: {
+                                    id: user.id 
+                                }
+                            }, 
+                            product: {
+                                connect: {
+                                    id: product[index]
+                                }
+                            }, 
+                            count: {
+                                connect: {
+                                    id: countId.id
+                                }
+                            },
+                            cart: {
+                                connect: {
+                                    id: cart[index]
+                                }
+                            }
+                        });
+                        console.log(payment);
+                        return payment;
+                        // return true;
+                    })
+                    // return prisma.payment({ id });
+                } catch (error) {
+                    console.log(error);
+                    return false; 
+                }
+            }
+        }
+        
+    }
+}
--- src/api/Post/seeFeed/seeFeed.js
+++ src/api/Post/seeFeed/seeFeed.js
@@ -15,6 +15,37 @@ export default {
         },
         orderBy: "createdAt_DESC"
       });
+    },
+    // postsOffsetPagination: async (root, {first, after}, ctx, info) => {
+    //   const posts = await prisma.posts({
+    //     orderBy: "createdAt_DESC",
+    //     first: 4
+    //   })
+    //   const index = posts.map(m => m.id).indexOf(after) + 1
+    //   return posts.slice(index, index + 1)
+    // }
+  },
+  Mutation: {
+    seeFeedAll: async(_, args) => {
+      const { id, first, skip, after } = args;
+      
+      if( id !== undefined) {
+        // const index = await prisma.posts.map(m => m.id).indexOf(skip)+1
+        const posts = await prisma.posts({ 
+          where: {id},
+          orderBy: "createdAt_DESC",
+          first,
+          skip
+        })
+        // .slice(index, index+1);
+        return posts;
+      }
+      
+      return prisma.posts({
+        orderBy: "createdAt_DESC",
+        first,
+        skip
+      })
     }
   }
 };
