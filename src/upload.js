8b36738a4688608b1166ebbbdf375273a876168a
milkteahot <greenegoode11@gmail.com>
2020-04-01 01:52:16 +0900

    metadata

--- src/upload.js
+++ src/upload.js
@@ -1,30 +1,29 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-// import aws from "aws-sdk";
+import aws from "aws-sdk";
 import { response } from "express";
 import path from 'path'
 
-var AWS = require('aws-sdk');
-AWS.config.credentials = new AWS.EC2MetadataCredentials({
-  httpOptions: { timeout: 6000 }
-});
+// var AWS = require('aws-sdk');
+// AWS.config.credentials = new AWS.EC2MetadataCredentials({
+//   httpOptions: { timeout: 6000 }
+// });
 
-const s3 = new AWS.S3({
+const s3 = new aws.S3({
   accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_SECRET,
   region: "ap-northeast-2"
 });
 
-
-
 const upload = multer({
   storage: multerS3({
     s3: s3,
+    acl: 'public-read',
     bucket: "catcher-test2",
-    // metadata: function(req, file, cb) {
-    //   cb(null, { fieldName: file.fieldName });
-    // },
-    key: function(req, file, cb) {
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
       let extension = path.extname(file.originalname);
       cb(null, Date.now().toString()+extension);
     }
