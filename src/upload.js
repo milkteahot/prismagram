3ac64a4a1ec715b3b61676db68721a9de71193cc
milkteahot <greenegoode11@gmail.com>
2020-04-01 00:53:28 +0900

    s3 업로드 성공공

--- src/upload.js
+++ src/upload.js
@@ -1,12 +1,7 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
-var path = require('path');
-
-// var AWS = require('aws-sdk');
-// AWS.config.credentials = new AWS.EC2MetadataCredentials({
-//   httpOptions: { timeout: 4000 }
-// });
+import { response } from "express";
 
 const s3 = new aws.S3({
   accessKeyId: process.env.AWS_KEY,
@@ -14,87 +9,33 @@ const s3 = new aws.S3({
   region: "ap-northeast-2"
 });
 
- //Storage multer config
-// let storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//       cb(null, "uploads/");
-//   },
-//   filename: (req, file, cb) => {
-//       cb(null, `${Date.now()}_${file.originalname}`);
-//   },
-//   fileFilter: (req, file, cb) => {
-//       const ext = path.extname(file.originalname)
-//       if(ext !== '.mp4' || ext !== '.png'|| ext !== '.jpg' || ext !== '.gif') {
-//           return cb(res.status(400).end('only mp4, png, jpg is allowed'), false);
-//       }
-//       cb(null, true)
-//   }
-// });
-
-// const upload = multer({ storage: storage }).single("file");
-
-// const upload = multer({ dest: "uploads/" });
-// const upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     acl: "public-read-write",
-//     bucket: "catcher-test2",
-//     metadata: function(req, file, cb) {
-//       cb(null, { fieldName: file.fieldName });
-//     },
-//     key: function(req, file, cb) {
-//       cb(null, Date.now().toString());
-//     }
-//   })
-// });
-
-let s3storage = multerS3({
-  s3: s3,
-  bucket: "catcher-test2",
-  key: function(req, file, cb) {
-    let extension = path.extname(file.originalname);
-    let basename = path.basename(file.originalname, extension);
-    cb(null, Date.now().toString());
-  },
-  acl: 'public-read-write',
-  contentDisposition: 'attachment',
-  serverSideEncryption: 'AES256'
-})
-
-// export const uploadMiddleware = upload.single("file");
-const upload = multer({ storage: s3storage });
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: "catcher-test2",
+    // metadata: function(req, file, cb) {
+    //   cb(null, { fieldName: file.fieldName });
+    // },
+    key: function(req, file, cb) {
+      console.log(file);
+      cb(null, Date.now().toString());
+    }
+  }),
+  limits: {fileSize: 5 * 1024 * 1024}
+});
 
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res, err) => {
 
-  
-  const {
-    file 
-    // file: { location }
-  } = req;
-  // const location = req.file.location
-  console.log(file);
-  // console.log(req.file.location);
-  // console.log(res.req.file.location);
-  // console.log(req.file.path);
-  // console.log(res.req.file.path);
-  // console.log(location);
-
-  // console.log(path);
-  // res.end();
-  //res.json({path: "jlkjlk"});
-  // res.json({ path });
-  // if(err) {
-  //   return res.json({ success: false, err })
-  // }
-  try {
-  console.log(res.req.file.location)
-  return res.json({ location })
-  }catch(e){
-    console.log(e)
+  try{
+    const {file: { location }} = req;
+    // console.log("req.file: ", req.file)
+    res.json({ location });  
+  } catch (err) {
+    console.log(err);
+    response(res, 500, "이미지 업로드중 서버 에러")
   }
-
-  // res.end();
   
 };
