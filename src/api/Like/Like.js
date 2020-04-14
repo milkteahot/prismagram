aa472de6739bf2db6317e7d13179858eac151bb3
milkteahot <greenegoode11@gmail.com>
2020-04-14 21:13:33 +0900

    toggleBookmark

--- src/api/Like/Like.js
+++ src/api/Like/Like.js
@@ -3,6 +3,7 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Like: {
     post: ({ id }) => prisma.like({ id }).post(),
+    product: ({ id }) => prisma.like({ id }).product(),
     user: ({ id }) => prisma.like({ id }).user()
   }
 };
