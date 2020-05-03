a3adeafd1f55423ad890c41f31a1c828603b6fba
milkteahot <greenegoode11@gmail.com>
2020-05-03 16:34:55 +0900

    getSeatValue block반환,seat상태값 업데이트

--- src/api/Block/getSeatValue/getSeatValue.js
+++ src/api/Block/getSeatValue/getSeatValue.js
@@ -5,46 +5,20 @@ export default {
     Mutation: {
         getSeatValue: async(_, args, {request }) => {
             isAuthenticated(request);
-            const { seatValue, seatNum, blockId } = args;
+            const { seatValue, blockId } = args;
             const {user} = request;
 
-            //seatNum 크기만큼의 빈 배열 만들기
-            var seatArr = new Array(seatNum);
-            for(var i=0;i<seatArr.length;i++){
-                seatArr[i] = new Array(seatNum);
-            }
-            /*
-            //전체seat크기만큼 배열과 입력받은 인덱스값 비교
-            for(var i=0;i<seatArr.length;i++){
-
-                for(var j=0;j<seatValue.length;)
-                    if(seatArr[i] == seatValue[j]){
-                        prisma.updateSeat({
-                            where: {
-                                id: seatArr
-                            },
-                            data: {
-                                number:2
-                            }
-                        })
-                        j++;
-                    }
-            }
-*/
-            const seatAll = {
-                AND: [
-                    {block: {
+            const blockSeat = await prisma.seats({
+                where: {
+                    block:{
                         id: blockId
-                    }},
-                ]
-            }
-            const blockSeat = await prisma.seats(seatAll)
-            const blockSeatId = blockSeat.id
-            console.log(blockSeat) //true
-            console.log(blockSeatId) //undefined
+                    }
+                }
+            })
+            const blockSeatId = blockSeat.map(item=>item.id)
+            // console.log(blockSeat) //seat 정보
+            // console.log(blockSeatId) //undefined
             seatValue.map(async (item, index) => {
-                // var j=0;
-                // if(item == seatValue[j]){
                     await prisma.updateManySeats({
                         where: {
                             id:blockSeatId[item]
@@ -53,56 +27,9 @@ export default {
                             number: 2
                         }
                     })
-                    // j++;
-                // }
             })
-
-            
-
-            /* var seatRow = Math.sqrt(seatNum);
-
-            var row = new Array(seatRow);
-            for(var i=0;i<row.length;i++){
-                row[i] = new Array(seatRow);
-            }
-            // for(var i=0;i<row.length;i++){
-            //     for(var j=0;j<seatRow;j++){
-            //         row[j][i] = 1;
-            //     }
-            // }
-            var seats = row;
-
-            const block = await prisma.createBlock({
-                creator: {
-                    connect: {
-                        id: user.id
-                    }
-                },
-                funding: {
-                    connect: {
-                        id: fundingId
-                    }
-                },
-                seatNum: seatNum
-            });
-            // seats.forEach(
-            //     async seat =>
-            
-            for(var i=0;i<seatRow;i++){
-                seats.forEach(
-                async seat =>
-                await prisma.createSeat({
-                    number: 1,
-                    block: {
-                        connect: {
-                            id: block.id
-                        }
-                    }
-
-                })
-              )
-            } */
-            return true;
+            const block = await prisma.block({id:blockId})
+            return block;
         }
     }
 }
