a1bcaf4cc02f803e0cffb1ef44f5212091dd8a11
milkteahot <greenegoode11@gmail.com>
2020-06-18 15:20:01 +0900

    Update editPost query for Content

--- src/api/Post/editPost/editPost.js
+++ src/api/Post/editPost/editPost.js
@@ -4,56 +4,61 @@ const DELETE = "DELETE";
 const EDIT = "EDIT";
 
 export default {
-    Mutation: {
-        editPost: async(_, args, {request, isAuthenticated}) => {
-            isAuthenticated(request);
-            const { id, caption, title, action } = args;
-            const { user } = request;
-            const post = await prisma.$exists.post({ id, user: {id: user.id }});
-            if(post) {
-                if(action === EDIT) {
-                    return prisma.updatePost({
-                        data: {caption, title}, 
-                        where: {id}
-                        }); 
-                } else if(action === DELETE) {
-                    return prisma.deletePost({ id });
-
-                }
-            } else {
-                throw Error("You can't do that");
-            }
-            
-        },
-        editTag: (_, args) => {
-            const { postId, tagId, text } = args;
-            try {
-                text.map(async (item, index) => {
-                    if(tagId !== undefined) {
-                        await prisma.updateTag({
-                            where: {
-                                id: tagId[index]
-                            },
-                            data: {
-                                text: item
-                            }
-                        });
-                    } else {
-                        await prisma.createTag({
-                            text: item,
-                            post: {
-                                connect: {
-                                    id: postId
-                                }
-                            }
-                        });
-                    }
-                });
-                return true;
-            } catch {
-                return false;
-            }
+  Mutation: {
+    editPost: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const {
+        id,
+        caption,
+        title,
+        action,
+        mainCategory,
+        subCategory,
+        isValidated
+      } = args;
+      const { user } = request;
+      const post = await prisma.$exists.post({ id, user: { id: user.id } });
+      if (post) {
+        if (action === EDIT) {
+          return prisma.updatePost({
+            data: { caption, title, mainCategory, subCategory, isValidated },
+            where: { id }
+          });
+        } else if (action === DELETE) {
+          return prisma.deletePost({ id });
         }
-
+      } else {
+        throw Error("You can't do that");
+      }
+    },
+    editTag: (_, args) => {
+      const { postId, tagId, text } = args;
+      try {
+        text.map(async (item, index) => {
+          if (tagId !== undefined) {
+            await prisma.updateTag({
+              where: {
+                id: tagId[index]
+              },
+              data: {
+                text: item
+              }
+            });
+          } else {
+            await prisma.createTag({
+              text: item,
+              post: {
+                connect: {
+                  id: postId
+                }
+              }
+            });
+          }
+        });
+        return true;
+      } catch {
+        return false;
+      }
     }
-}
+  }
+};
